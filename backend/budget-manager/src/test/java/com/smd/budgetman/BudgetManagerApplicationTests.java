94b5e3ce0d2752418a7c80cad610ffa757703f5e
Andrei Gorneanu <andreigorneanu@gmail.com>
2026-05-14 07:50:47 +0000

    [Backend] Initial commit - empty Spring Boot project

---
+++ backend/budget-manager/src/test/java/com/smd/budgetman/BudgetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smd.budgetman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
